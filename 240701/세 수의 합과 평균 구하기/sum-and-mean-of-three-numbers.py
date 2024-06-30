d59569028e1343a370fb29b5fd76a83dc96f8dd9
koh9904 <80735225+koh9904@users.noreply.github.com>
2024-07-01 01:23:07 +0900

    Title: 세 수의 합과 평균 구하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240701/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 240701/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -1,5 +1,3 @@
-a = input().split()
-b = int(a[0]) + int(a[1]) + int(a[2])
-c = (int(a[0]) + int(a[1]) + int(a[2])) / 3
-print(b)
-print(f"{c:.0f}")
+a, b, c = map(int, input().split())
+print(f"{a+b+c}")
+print(f"{(a+b+c)/3:.0f}")
