32d77c761fa4c0438ca237d258dab0e47dd7e5a8
koh9904 <80735225+koh9904@users.noreply.github.com>
2024-07-04 01:38:37 +0900

    Title: 369 게임, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/369 게임/369-game.py
+++ 240704/369 게임/369-game.py
@@ -2,5 +2,7 @@ n = int(input())
 for i in range (1,n+1):
     if i%3 == 0 :
         print(0, end=" ")
+    elif i == 3 or i == 6 or i == 9 :
+        print(0, end=" ")
     else :
         print(i, end=" ")
